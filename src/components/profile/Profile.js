5bdd3ff3c81a1baec8fa763e994907cf45c716a4
Paulo Henrique <pauloh1288@gmail.com>
2020-04-21 20:20:49 -0300

    feat: add Profile component

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export default function Profile() {
+  return (
+    <div className="Profile-wrapper">
+      <h1>Matt foo</h1>
+      <h2>Back-end developer</h2>
+      <p>
+        Core team member in Bar. I write about back0end world and other
+        things
+      </p>
+    </div>
+  );
+}

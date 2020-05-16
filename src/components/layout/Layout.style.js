c1f789729f943fcae26f34d79b380f4f75754a61
Paulo Henrique <pauloh1288@gmail.com>
2020-05-16 19:33:24 -0300

    chore: add editorconfig, eslint, husky, lint-staged and commitlint

---
+++ src/components/layout/Layout.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LayoutRoot = styled.section`
+  display: flex;
+`;
+
+export const LayoutMain = styled.main`
+  background: #16202c;
+  min-height: 100vh;
+  padding-right: 3.75rem;
+  padding-left: 20rem;
+  width: 100%;
+`;

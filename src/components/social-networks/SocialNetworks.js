a91e1fbca683c3520590ec8bda78a83704f3a02f
Paulo Henrique <pauloh1288@gmail.com>
2020-05-17 11:27:19 -0300

    refactor: changes icons import to named imports

--- src/components/social-networks/SocialNetworks.js
+++ src/components/social-networks/SocialNetworks.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as S from './SocialNetworks.style';
 import {
-  Github,
-  Twitter,
-  Instagram,
-  Youtube,
-  Facebook,
+  Github as GithubIcon,
+  Twitter as TwitterIcon,
+  Instagram as InstagramIcon,
+  Youtube as YoutubeIcon,
+  Facebook as FacebookIcon,
 } from 'styled-icons/boxicons-logos';
 
 const socialNetworks = [
@@ -13,31 +13,31 @@ const socialNetworks = [
     id: '82a9d946',
     link: 'https://github.com/phenrique7',
     label: 'GitHub',
-    icon: Github,
+    icon: GithubIcon,
   },
   {
     id: 'dfc1d736',
     link: 'https://twitter.com/__phenrique7',
     label: 'Twitter',
-    icon: Twitter,
+    icon: TwitterIcon,
   },
   {
     id: 'fd3cb848',
     link: 'https://www.instagram.com/phenrique___',
     label: 'Instagram',
-    icon: Instagram,
+    icon: InstagramIcon,
   },
   {
     id: '2ab14d8c',
     link: 'https://www.youtube.com/channel/UCgm5bwMbxHDiF_RdC-aWg6A',
     label: 'Youtube',
-    icon: Youtube,
+    icon: YoutubeIcon,
   },
   {
     id: '800a0881',
     link: 'https://www.facebook.com/PHenrique1288',
     label: 'Facebook',
-    icon: Facebook,
+    icon: FacebookIcon,
   },
 ];
 

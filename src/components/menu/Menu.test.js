d723d2301a52fdb5614a9acb83409c52476f388e
Paulo Henrique <pauloh1288@gmail.com>
2020-05-17 15:01:40 -0300

    test: add tests for Menu component

---
+++ src/components/menu/Menu.test.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import Menu from './Menu';
+
+describe('<Menu /> test suite', () => {
+  it('should render the component correctly', () => {
+    render(<Menu />);
+
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('About')).toBeInTheDocument();
+  });
+});

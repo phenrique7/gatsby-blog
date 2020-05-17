83055fc550e585c6fc3e64c4eaf74c644044a13d
Paulo Henrique <pauloh1288@gmail.com>
2020-05-16 22:58:42 -0300

    improvement: put SocialNetworks component in Sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Profile from '../profile/Profile';
+import SocialNetworks from '../social-networks/SocialNetworks';
 import * as S from './Sidebar.style';
 
 export default function Sidebar() {
   return (
     <S.SidebarRoot>
       <Profile />
+      <SocialNetworks />
     </S.SidebarRoot>
   );
 }

b041bc85db62a91d9c81a820cfaf112b52487d4a
Paulo Henrique <pauloh1288@gmail.com>
2020-05-24 09:47:56 -0300

    chore: inserts site layout into blog post template component

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
+import Layout from '../components/layout/Layout';
+import SEO from '../components/seo/SEO';
 import { Frontmatter } from '../utils/types';
 
 interface MarkdownRemark {
@@ -15,10 +17,11 @@ export default function BlogPost({ data }: BlogPostProps) {
   const post = data.markdownRemark;
 
   return (
-    <>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
       <h1>Title: {post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </>
+    </Layout>
   );
 }
 

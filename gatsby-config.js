cbe9a5b2ae1fda0ad2fc19265b5c5c5f5816acfb
Paulo Henrique <pauloh1288@gmail.com>
2020-05-24 10:40:48 -0300

    chore: configures gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,14 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    // it needs to be the first one to work with gatsby-remark-images
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'uploads',
+        path: `${__dirname}/static/assets/images`,
+      },
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -23,7 +31,27 @@ module.exports = {
         path: `${__dirname}/posts`,
       },
     },
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 960,
+              linkImagesToOriginal: false,
+            },
+          },
+          'gatsby-remark-lazy-load',
+        ],
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {

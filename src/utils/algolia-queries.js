35c8cd6edfb9f3fa814b0d2857d26f42ce175701
Paulo Henrique <pauloh1288@gmail.com>
2020-05-31 16:57:13 -0300

    chore: adds netlifycms integration

--- src/utils/algolia-queries.js
+++ src/utils/algolia-queries.js
@@ -41,7 +41,7 @@ const queries = [
   {
     query: PostsQuery,
     transformer: ({ data }) => flatten(data.posts.edges),
-    indexName: 'Posts',
+    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
     settings: {
       attributesToSnippet: ['excerpt:20'],
     },

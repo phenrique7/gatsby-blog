b138f0ff49aec53b51ad5ef77e06fdddaa7ffb1b
Paulo Henrique <pauloh1288@gmail.com>
2020-05-30 21:55:25 -0300

    refactor(pagination): replaces border-top with box-shadow

--- src/components/pagination/Pagination.style.ts
+++ src/components/pagination/Pagination.style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const PaginationRoot = styled.section`
   display: flex;
   align-items: center;
-  border-top: 1px solid var(--borders);
+  box-shadow: 0 -1px 0 0 var(--borders);
   color: var(--texts);
   padding: 1.5rem 3rem;
   justify-content: space-between;
